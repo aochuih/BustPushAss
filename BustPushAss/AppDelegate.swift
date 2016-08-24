fd8d2a977e48860d1387c7263b216b310bfba66b
李会超 <lihuichao@meituan.com>
2016-08-24 19:47:58 +0800

    first commit

---
+++ BustPushAss/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BustPushAss
+//
+//  Created by aochuih on 16/8/23.
+//  Copyright © 2016年 aochuih. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BustPushAss/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BustPushAss
+//
+//  Created by aochuih on 16/8/23.
+//  Copyright © 2016年 aochuih. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
